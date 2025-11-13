7d06d504ae79c4bc22bb20c9e15d2dd039f86216
dave 😎 <davidroymellors@gmail.com>
2025-11-13 20:33:56 +0200

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let path = match args.get(1) {
+        Some(p) => p,
+        None => panic!("No arguments were attached"),
+    };
+
+    let file = File::open(path);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
+            }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
